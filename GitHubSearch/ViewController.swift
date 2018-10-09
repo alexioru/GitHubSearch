c2ad290263e2455695cbca2b59c21995bf649646
Alexey Rodionov <mail@alexio.ru>
2018-10-09 14:09:59 +0700

    Added project files

---
+++ GitHubSearch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitHubSearch
+//
+//  Created by Alexey Rodionov on 10/9/18.
+//  Copyright © 2018 Alexey Rodionov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
